e6b47a773228af659af9b7f7d1f01b3d467c5118
fitriaaci <fitriaaci13@gmail.com>
2025-01-14 20:48:51 -0800

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,16 @@
+
+let nama = "Cahaya shifa fitria aji"; 
+let alamat = "kalimantan barat"; 
+let usia = "15";  
+let ttl = "jepara 24 september 2009"; 
+let gender = "perempuan"; 
+let agama = "islam"; 
+let email = "fitriaaci13@gmail.com";
+let berat = "45";
+let tinggi = "157";
+let hobi = "menggambar ";
+let cita = "dokter";
+let pekerjaan = "pelajar";
+let sekolah = "SMK N 1 SAYUNG";
+
+document.write("nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + "tahun, saya lahir di " + ttl + "jenis kelamin saya adalah " + gender + "saya beragama " + agama + "alamat email saya adalah " + email + "saya memiliki berat badan " + berat +"kg"+ "dengan tinggi " + tinggi + "saya sangat senang " + hobi + "karena saya memiliki cita cita " + cita + "saya saat adalah seorang " + pekerjaan + "yang bersekolah di " + sekolah + "pada jurusan " + jurusan);
